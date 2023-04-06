37e24fd75521f2762f1ba0aa88e06f88e08d932c
Vinicius Dos Santos Grana <54106581833@info.local>
2023-04-06 09:02:43 -0300

    3 exercicios

---
+++ Login/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine($"Digite o usuário: ");
+string usuario = Console.ReadLine();
+Console.WriteLine($"Digite sua senha: ");
+string senha = Console.ReadLine();
+
+
+while (usuario == senha)
+{
+    Console.WriteLine($"Senha incorreta ");
+    senha = Console.ReadLine();
+    
+}
+ Console.WriteLine($"Bem vindo!");
+ 
+
+
+
---
+++ nota/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine($"Digite uma nota de 0 a 10");
+float nota = float.Parse(Console.ReadLine());
+
+
+while (nota < 0 || nota> 10)
+{
+    Console.WriteLine($"Nota invalida");
+    nota = float.Parse(Console.ReadLine());
+}
+Console.WriteLine($"Nota valida");
+
+
+
